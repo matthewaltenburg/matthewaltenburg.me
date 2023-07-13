6add45e30940430f016d07fecb0a77bca7371635
Matthew Altenburg <mattaltenburg@gmail.com>
2023-07-13 06:44:23 +0000

    add bio changes

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -1,112 +1,43 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import Layout from '../components/layout';
 
-const bio = () => {
+const Bio = () => {
   return (
     <Layout>
       <div className="content-box clearfix">
         <div className="about-page">
           <h1>Biography</h1>
           <p className="sub">
-            “The best way to get to know someone is to take the time to
-            understand their journey.”
+            “Understanding someone's journey is the best way to truly know them.”
           </p>
-          <h2>Here is mine.</h2>
+          <h2>My Journey</h2>
           <p>
-            Where I am today all started as a kid when in 5th grade I got my
-            first computer which was a Commodore 64. Never interested in
-            stealing but in love with the puzzle of how things worked, in this
-            case, how this new computer was able to control the AT&T phone
-            exchanges. I used it to make free phone calls and to prank call the
-            White House. <br />
-            <br />
-            Unfortunately, not the wisest thing to do because back in the 80s,
-            there was an unreasonable fear of hacking. Because of this obsession
-            with my computers and the somewhat anti-social behaviour I was kept
-            away from any computers until early adulthood. <br /> <br />I was
-            kept busy with competitive swimming and became somewhat of a loner.
-            I think by today’s standers, I may have been diagnosed with
-            broad-spectrum autism. But at the time, it was classified as a
-            learning disability.
+            My interest in technology sparked when I got a Commodore 64 in 5th grade. This led to a life-long fascination with understanding how things work. However, due to some unconventional behaviors, I was distanced from computers until adulthood, filling my time with competitive swimming instead.
           </p>
           <h2>Early Adulthood</h2>
           <p>
-            Towards the end of high school, it became pretty clear that I was
-            not naturally able to communicate or develop personal relationships
-            similar to other teenagers. <br />
-            <br />
-            Not wanting to be alone, I chose the most challenging career I could
-            imagine that would force me to develop communication skills, and
-            that was hairdressing. <br />
-            <br />
-            Hairdressing was not my passion. Science, math and computers were,
-            but I knew that I would find myself alone and lost in these fields.
-            Happy with what I was doing but unable to communicate effectively
-            with others. <br />
-            <br />
-            Because of my attention to detail and need to find patterns in what
-            I was doing, I became a very skilled hairdresser and started working
-            with the best hairdressers in the world, Vidal Sassoon, Toni and
-            Guy, Gram Webb, to name a few. My personality and personal skill
-            soon followed; although I was pretty awkward for the first few
-            years, nobody seemed to care as long as their hair looked good.{' '}
-            <br />
-            <br />
-            During this time is where I met my first wife and had kids, and
-            develop several friendships.
+            I struggled with social skills in high school. To overcome this, I chose hairdressing - a career that demands strong communication. This decision proved fruitful as I honed my skills and got opportunities to work with industry giants. During this phase, I also started a family.
           </p>
           <h2>University</h2>
           <p>
-            After about five years of hairdressing, and because I now felt that
-            I could communicate much better, I left and went to the University
-            of Central Florida to study Chemistry. <br />
-            <br />
-            Here is where I felt at home study Calculus, Physics and Chemistry.
-            I love the research and the community. I also started getting
-            involved with Linux and Programming again; all was good. <br />
-            <br />
-            But eventually, I graduated and could not afford to stay. I had a
-            family, responsibilities and started working in a Phosphate mine as
-            a chemist and a laboratory technician, which I found incredibly
-            dull.
+            Having built confidence in my social skills, I ventured back to my first love - academia. I pursued a degree in Chemistry, where I could indulge my passions for science, math, and computers. Post-graduation, I worked as a chemist and laboratory technician.
           </p>
           <h2>Starting a Business</h2>
           <p>
-            Needing new challenges and have two very different skillsets,
-            Chemistry and Hairdressing, I started my business specialing in hair
-            color. At first, it was just me and eventually a salon and a team.{' '}
-            <br /> <br />
-            I enjoyed this and the challenges it brought, but I found myself
-            programming the booking system, website, and server for fun. <br />
-            <br />
-            My love of computers and programming from my childhood had never
-            gone.
+            Leveraging my unique skill sets in Chemistry and Hairdressing, I established a business specializing in hair color. Throughout this, my fondness for computers persisted, and I found joy in creating digital solutions for my business.
           </p>
-          <h2>Change Careers and Skillsets</h2>
+          <h2>Pivoting Careers</h2>
           <p>
-            After more than 22 years working as a Chemist, Hairdresser, and
-            business owner, I started studying and working with computers,
-            drones and AI in 2012. <br />
-            <br />I didn’t go to school but instead sought out professors,
-            industry leaders, and people technology community meetups and talked
-            to them personally. I watched a lot of Youtube and read several
-            scientific papers. Practice coding in python, c++, and javascript
-            and revised my Linear Algebra and Calculus skills from my university
-            days. I learned how to use Matlab, bash, vim, and several other
-            tools. I did this because I had projects I wanted to work on.
+            After over two decades juggling roles as a Chemist, Hairdresser, and business owner, I returned to the world of computers in 2012. Instead of traditional education, I pursued knowledge independently, networking with industry leaders, and self-studying programming languages and tools.
           </p>
-          <h2>Drones, Computer Vision and AI</h2>
+          <h2>Current Focus</h2>
           <p>
-            This is where the fascination that I had with computers in 5th grade
-            bring me to where I am today. When I find myself working with
-            computers to control drones and teaching them to see. The main
-            projects I work on are ArduPilot, OpenCV and Pytorch. To find out
-            more please see my projects page.
+            Now, my journey has brought me to the intersection of drones, computer vision, and AI, echoing my childhood fascination with computers. I am currently involved in projects with ArduPilot, OpenCV, and Pytorch. Please check out my projects page for more details.
           </p>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default bio
+export default Bio;
